01aa23ab5cbf78db10adaa3f6200c4bc23c54595
Max Nilsson <t0themax@yahoo.se>
2023-07-02 09:58:33 +0200

    Update sagt-om-oss.js

--- src/pages/sagt-om-oss.js
+++ src/pages/sagt-om-oss.js
@@ -102,10 +102,10 @@ const MappiaSagtOmOssPage = () => {
                         för eleverna att minnas vad de har lärt sig.
                       </p>
                       <p>
-                        - Hur jag än har presenterat bokstäver tidigare har flera
-                        av eleverna haft svårt att minnas dem. Med ljudfigurerna
-                        har det blivit lättare att komma ihåg. De hittar
-                        bokstaven lite snabbare i minnet, fortsätter Ann.
+                        - Hur jag än har presenterat bokstäver tidigare har
+                        flera av eleverna haft svårt att minnas dem. Med
+                        ljudfigurerna har det blivit lättare att komma ihåg. De
+                        hittar bokstaven lite snabbare i minnet, fortsätter Ann.
                       </p>
                       <p>
                         - Framför allt blir det lustfyllt och prestigelöst för
@@ -131,6 +131,59 @@ const MappiaSagtOmOssPage = () => {
                       <p>
                         Ann Björnsson, Speciallärare Elmeskolan, Älmhult. Text
                         Fredrik Skolfman.
+                      </p>
+                    </div>
+                    <StaticImage
+                      src="../images/sagtomoss.illustration1.png"
+                      alt="Hej"
+                    />
+                  </div>
+                </section>
+                <section class="sagtomoss">
+                  <div class="card">
+                    <div>
+                      <h2>Barnen har blivit så nyfikna!</h2>
+                      <p>
+                        - Jag var intresserad av om fler elever skulle få hjälp
+                        att knäcka läskoden om vi använde Havsäventyret. Förut
+                        tragglade och stampade vi lite i olika metoder, så vi
+                        ville gärna prova något nytt. Barnen idag vill ju att
+                        det ska vara roligt och att de ska känna att det går
+                        framåt, säger Anna.Anna berättar att eleverna är nyfikna
+                        på ljudfigurerna och berättelserna, vilket gör att de
+                        vill gå vidare och lära sig mer. Därför ser de också
+                        fram emot nästa lektion.
+                      </p>
+                      <p>
+                        - Det blir inte bara platta bokstäver att jobba med,
+                        utan nu har vi personligheter som man kan prata om. Det
+                        blev många spännande samtal och man pratade om bokstäver
+                        som om de vore personer och de betydde något för barnen.
+                        De startade mycket diskussioner om vem som var kompisar
+                        med vem, vad de olika figurerna tycker om osv,
+                        fortsätter Anna. Till höstterminen 2021 kommer Ängdala
+                        skolor att arbeta med Havsäventyret för alla femåringar,
+                        så att de får en bra grund att stå på.{" "}
+                      </p>
+                      <p>
+                        - Motoriskt så är inte de små barnen redo för att sitta
+                        och skriva bokstäver vid en bänk. Här kan de stå, sitta
+                        eller ligga på golvet. Bilderna är så roliga och
+                        finurliga att barnen fastnar i det och vill jobba
+                        vidare. Vi kommer också använda Havsäventyret i mindre
+                        grupper för de lite äldre barnen, säger Anna. Anna vill
+                        gärna poängtera att det är viktigt att inte prata om hur
+                        bokstäverna låter i alfabetet eftersom det kan förvirra
+                        språksvaga elever. Detta är en kunskap som inte alla
+                        lärare fått med sig från sin utbildning. Det är viktigt
+                        att börja på rätt sätt - med språkljuden. Mitt råd till
+                        lärare är att bara köra! Havsäventyret är lätt att
+                        använda och vill man så går det utmärkt att komplettera
+                        med andra läroböcker, avslutar Anna.
+                      </p>
+                      <p>
+                        Anna Aspenlund, Biträdande rektor och Specialpedagog,
+                        Ängdalaskolor, Höllviken.Text: Fredrik Skolfman.
                       </p>
                     </div>
                     <StaticImage
